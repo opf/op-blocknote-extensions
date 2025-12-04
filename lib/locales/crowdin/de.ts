d06aa55797c62f36e47da3cca2164ca2f684d9db
Judith Roth <j.roth@openproject.com>
2025-12-04 14:52:05 +0100

    New Crowdin updates (#73)
    * New translations en.ts (French)
    
    * New translations en.ts (Spanish)
    
    * New translations en.ts (German)
    
    * New translations en.ts (Dutch)
    
    * New translations en.ts (Portuguese)
    
    * New translations en.ts (Ukrainian)
    
    * New translations en.ts (German)
    
    * New translations en.ts (French)
    
    * New translations en.ts (Spanish)

---
+++ lib/locales/crowdin/de.ts
@@ -0,0 +1,33 @@
+export const en = {
+  translation: {
+    "slashMenu": {
+      "title": "Vorhandenes Arbeitspaket verlinken",
+      "subtext": "Link zu einem einzelnen Arbeitspaket hinzufügen",
+      "aliases": {
+        "workpackage": "Arbeitspaket",
+        "work package": "work package",
+        "wp": "wp",
+        "link": "link"
+      }
+    },
+    "search": {
+      "label": "Bestehendes Arbeitspaket verlinken",
+      "placeholder": "Nach ID oder Betreff suchen",
+      "dropdownAriaLabel": "Suchergebnisse (Arbeitspakete)"
+    },
+    "unavailableWorkPackage": {
+      "loading": {
+        "header": "Lädt",
+        "message": "Bitte warten"
+      },
+      "unauthorized": {
+        "header": "Das verlinkte Arbeitspaket ist nicht verfügbar",
+        "message": "Aufgrund fehlender Berechtigungen kann es nicht angezeigt werden"
+      },
+      "error": {
+        "header": "Fehler",
+        "message": "Arbeitspaket konnte nicht geladen werden"
+      }
+    }
+  }
+};
---
+++ lib/locales/crowdin/fr.ts
@@ -0,0 +1,33 @@
+export const en = {
+  translation: {
+    "slashMenu": {
+      "title": "Lien vers un lot de travaux existant",
+      "subtext": "Ajouter un lien dynamique à un lot de travaux unique",
+      "aliases": {
+        "workpackage": "workpackage",
+        "work package": "lot de travaux",
+        "wp": "wp",
+        "link": "lien"
+      }
+    },
+    "search": {
+      "label": "Lien vers un lot de travaux existant",
+      "placeholder": "Rechercher l'ID ou le sujet du lot de travaux",
+      "dropdownAriaLabel": "Résultats de la recherche de lots de travaux"
+    },
+    "unavailableWorkPackage": {
+      "loading": {
+        "header": "Сhargement",
+        "message": "Veuillez patienter"
+      },
+      "unauthorized": {
+        "header": "Le lot de travaux lié n'est pas disponible",
+        "message": "Vous n'avez pas l'autorisation de voir ce contenu"
+      },
+      "error": {
+        "header": "Erreur",
+        "message": "Impossible de charger le lot de travaux"
+      }
+    }
+  }
+};
