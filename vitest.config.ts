7d9632b84b8ddb46a3706782cb819b63360bff1c
ihordubas99 <dubasihor25@gmail.com>
2026-03-06 13:48:48 +0200

    chore(test): remove jsdom, split test configs into unit and browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
     include: ['test/**/*.test.ts', 'test/**/*.test.tsx'],
-    exclude: ['test/**/*.browser.test.tsx'],  
+    exclude: ['test/**/*.browser.test.tsx'],
   },
 })
